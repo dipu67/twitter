79c01eef43e2ce3dc6a990a4bd3d39a5d338845f
Dipu Mia <112048792+dipu67@users.noreply.github.com>
2025-09-08 00:22:23 +0600

    Update app.js

--- app.js
+++ app.js
@@ -12,14 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 async function userProfile(userName) {
   const browser = await puppeteer.launch({
     executablePath: "/usr/bin/chromium",
-    args: [
-       '--no-sandbox',
-    '--disable-setuid-sandbox',
-    '--disable-dev-shm-usage',
-    '--disable-http2',
-    '--disable-features=IsolateOrigins,site-per-process'
-       
-      ],
+
   });
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
